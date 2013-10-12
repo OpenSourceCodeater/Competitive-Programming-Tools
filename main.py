245cadee999d3c11713db466ac07f3d1e23e81fd
Adrian Carballo <acarballo@emphasys-software.com>
2013-10-12 14:16:28 -0400

    Create main.py
    This is a simple python file for writing code for programming contests. The environment variable 'KANGOONIE' redirects the standard IO to local files when the variable is defined. Otherwise all IO operations are done using standard IO (i.e console)
    To get it to work you will need to define your environment variable, and create the files in.txt and out.txt in the same directory level as this file.
    Enjoy!

---
+++ main.py
@@ -0,0 +1,26 @@
+import sys
+import os
+
+inp = sys.stdin
+out = sys.stdout
+
+def main():
+    local = os.getenv('KANGOONIE', 0)
+    if local:
+        global inp, out
+        inp = sys.stdin = open('in.txt', 'r')
+        out = sys.stdout = open('out.txt', 'w')
+
+    run()
+
+    if local:
+        inp.close()
+        out.flush()
+        out.close()
+
+def run():
+    #TODO put your code here
+    print('Hello world')
+
+main()
+sys.exit(0)
