7bdd7d2d29c0ca6fb7e3d5deec33a768bed6b701
Adrian Carballo <acarballo07@users.noreply.github.com>
2013-11-24 13:22:55 -0500

    Update Segment Tree RMQ.py

--- data structures/Segment Tree RMQ.py
+++ data structures/Segment Tree RMQ.py
@@ -1,32 +1,39 @@
 class SegmentTreeNode:
         def __init__(self, l, r):
-            (self.left, self.right) = l, r
+            self.left = l
+            self.right = r
 
-        def merge(self, leftChild, rightChild):
-            if leftChild is None:
-                self.value = rightChild.value
-            elif rightChild is None:
-                self.value = leftChild.value 
+        def merge(self, left, right):
+            if left is None:
+                self.value = right.value
+            elif right is None:
+                self.value = left.value 
             else:
-                self.value = min(leftChild.value, rightChild.value)
+                self.value = min(left.value, right.value)
 
 class SegmentTree:
     def __init__(self, A):
-        self.length = len(A)
-        totalNodes = 2 * (2 ** math.ceil(math.log(self.length, 2)))
-        self.__tree = [None] * totalNodes
-        self.__build(1, 0, self.length - 1, A)
+        N = len(A)
+        self.length = N
+        power = math.ceil(math.log(N, 2))
+        total = 2 ** (power + 1)
+        self.__tree = [None] * total
+        self.__build(1, 0, N - 1, A)
 
     def __build(self, node, l, r, A):
         if l == r: #leaf node
             self.__tree[node] = SegmentTreeNode(l, r)
             self.__tree[node].value = A[l]
             return
-        (leftChild, rightChild, mid) = 2 * node, 2 * node + 1, (l + r) // 2
+        leftChild = 2 * node
+        rightChild = leftChild + 1
+        mid = (l + r) // 2
         self.__build(leftChild, l, mid, A)
-        self.__build(rightChild, mid+1, r, A)
+        self.__build(rightChild, mid + 1, r, A)
         self.__tree[node] = SegmentTreeNode(l, r)
-        self.__tree[node].merge(self.__tree[leftChild], self.__tree[rightChild])
+        L = self.__tree[leftChild]
+        R = self.__tree[rightChild]
+        self.__tree[node].merge(L, R)
 
     def __query(self, node, l, r, i, j):
         if l >= i and r <= j: 
@@ -34,15 +41,17 @@ class SegmentTree:
         elif j < l or i > r: 
             return None
         else:
-            (leftChild, rightChild, mid) = 2 * node, 2 * node + 1, (l + r) // 2
+            leftChild = 2 * node
+            rightChild = leftChild + 1
+            mid = (l + r) // 2
             L = self.__query(leftChild, l, mid, i, j)
-            R = self.__query(rightChild, mid+1, r, i, j)
+            R = self.__query(rightChild, mid + 1, r, i, j)
             temp = SegmentTreeNode(-1, -1) #dummy node
             temp.merge(L, R)
             return temp
 
     def query(self, i, j):
-        return self.__query(1, 0, self.length-1, i, j).value
+        return self.__query(1, 0, self.length - 1, i, j)
 
     def __update(self, node, l, r, i, v):
         if l == i and r == i:
@@ -50,10 +59,14 @@ class SegmentTree:
         elif i < l or i > r:
             return None
         else:
-            (leftChild, rightChild, mid) = 2 * node, 2 * node + 1, (l + r) // 2
+            leftChild = 2 * node
+            rightChild = leftChild + 1
+            mid = (l + r) // 2
             self.__update(leftChild, l, mid, i, v)
-            self.__update(rightChild, mid+1, r, i, v)
-            self.__tree[node].merge(self.__tree[leftChild], self.__tree[rightChild])
+            self.__update(rightChild, mid + 1, r, i, v)
+            L = self.__tree[leftChild]
+            R = self.__tree[rightChild]
+            self.__tree[node].merge(L, R)
 
     def update(self, i, value):
         self.__update(1, 0, self.length, i, value)
